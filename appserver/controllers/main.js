91e108b0bd4b25600473d2ef1542818930068101
Ashish-eer <pagadalaashish499@gmail.com>
2024-09-26 22:15:52 +0530

    Your descriptive commit message

--- appserver/controllers/main.js
+++ appserver/controllers/main.js
@@ -1,4 +1,9 @@
-module.exports.index = function(req,res){
-    res.render('index',{title:'Express'});
-   };
-   
+module.exports.index=function(req,res){
+    res.render('index', { title: 'About' });
+};
+module.exports.signin=function(req,res){
+    res.render('signin', { title: 'Location' });
+};
+module.exports.review=function(req,res){
+    res.render('review', { title: 'Review' });
+};
